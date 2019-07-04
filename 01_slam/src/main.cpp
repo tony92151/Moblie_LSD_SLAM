aef5918d29e410080a0e4fd91a448ef78291ab12
tony <tony92151@gmail.com>
2019-07-04 09:32:49 +0800

    01

---
+++ 01_slam/src/main.cpp
@@ -0,0 +1,116 @@
+// //  https://www.cnblogs.com/gaoxiang12/p/4652478.html
+// //  main.cpp
+// //  01_slam
+// //
+// // pcl1.9
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// //#include <opencv2/core/core.hpp>
+
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+
+
+// using namespace cv;
+
+// #include <pcl/io/pcd_io.h>
+// #include <pcl/point_types.h>
+
+// //Mat rgb;
+
+
+// typedef pcl::PointXYZRGBA PointT;
+// typedef pcl::PointCloud<PointT> PointCloud;
+
+// double camera_factor = 1000;
+// double camera_cx = 325.5;
+// double camera_cy = 253.5;
+// double camera_fx = 518.0;
+// double camera_fy = 519.0;
+
+// //#include <pcl/io/pcd_io.h>
+
+// //int main(int argc, const char * argv[]) {
+// //    //Eigen::In i ;
+// //    Mat rgb, depth;
+// //    // insert code here...
+// //    //cout << "Hello, World!\n";
+// //    //Mat rgb, depth;
+// //    rgb = imread( "./image/depth.png",-1);
+// //
+// //    PointCloud::Ptr cloud ( new PointCloud );
+// //
+// //}
+
+// // 主函数
+// int main( int argc, char** argv )
+// {
+//     // 读取./data/rgb.png和./data/depth.png，并转化为点云
+    
+//     // 图像矩阵
+//     cv::Mat rgb, depth;
+//     // 使用cv::imread()来读取图像
+//     // API: http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#cv2.imread
+//     rgb = cv::imread( "./image/rgb.png" );
+//     // rgb 图像是8UC3的彩色图像
+//     // depth 是16UC1的单通道图像，注意flags设置-1,表示读取原始数据不做任何修改
+//     depth = cv::imread( "./image/depth.png", -1 );
+    
+//     // 点云变量
+//     // 使用智能指针，创建一个空点云。这种指针用完会自动释放。
+//     PointCloud::Ptr cloud ( new PointCloud );
+//     // 遍历深度图
+//     for (int m = 0; m < depth.rows; m++)
+//         for (int n=0; n < depth.cols; n++)
+//         {
+//             // 获取深度图中(m,n)处的值
+//             ushort d = depth.ptr<ushort>(m)[n];
+//             // d 可能没有值，若如此，跳过此点
+//             if (d == 0)
+//                 continue;
+//             // d 存在值，则向点云增加一个点
+//             PointT p;
+            
+//             // 计算这个点的空间坐标
+//             p.z = double(d) / camera_factor;
+//             p.x = (n - camera_cx) * p.z / camera_fx;
+//             p.y = (m - camera_cy) * p.z / camera_fy;
+            
+//             // 从rgb图像中获取它的颜色
+//             // rgb是三通道的BGR格式图，所以按下面的顺序获取颜色
+//             p.b = rgb.ptr<uchar>(m)[n*3];
+//             p.g = rgb.ptr<uchar>(m)[n*3+1];
+//             p.r = rgb.ptr<uchar>(m)[n*3+2];
+            
+//             // 把p加入到点云中
+//             cloud->points.push_back( p );
+//         }
+//     // 设置并保存点云
+//     cloud->height = 1;
+//     cloud->width = cloud->points.size();
+//     cout<<"point cloud size = "<<cloud->points.size()<<endl;
+//     cloud->is_dense = false;
+//     pcl::io::savePCDFile( "./pointcloud.pcd", *cloud );
+//     // 清除数据并退出
+//     cloud->points.clear();
+//     cout<<"Point cloud saved."<<endl;
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int result;
+    int a = 2;
+    int b = 3;
+    result = a + b;
+    cout << result << endl;
+    return 0;
+}
---
+++ 01_slam/test/main.cpp
@@ -0,0 +1,10 @@
+#include "../include/add.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<add(2,5);
+}
