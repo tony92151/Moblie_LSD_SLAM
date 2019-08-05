75384d9234ac8179bf660da53cafba1f33ff87c0
tony92151 <tony92151@gmail.com>
2019-08-05 22:45:26 +0800

    test

--- 03_slam/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tony92151/Moblie_LSD_SLAM/03_slam
-# Build directory: /home/tony92151/Moblie_LSD_SLAM/03_slam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
